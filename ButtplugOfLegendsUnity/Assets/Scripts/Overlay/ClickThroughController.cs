8956880cd98e8b835a89dc186d0e8f0e10b230cb
Furimanejo <furimanejo@gmail.com>
2022-02-14 17:36:08 -0300

    Rework ClickThroughController to work with mouse event callbacks and not raycasts

--- ButtplugOfLegendsUnity/Assets/Scripts/Overlay/ClickThroughController.cs
+++ ButtplugOfLegendsUnity/Assets/Scripts/Overlay/ClickThroughController.cs
@@ -4,43 +4,20 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ClickThroughController : MonoBehaviour
+public class ClickThroughController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] OverlayController overlayController = null;
-    [SerializeField] EventSystem eventSystem = null;
-    [SerializeField] GraphicRaycaster graphicRaycaster = null;
-    bool status = true;
-
-    private void Start()
-    {
-        StartCoroutine(CheckRaycast());
-    }
 
     private void OnDisable()
     {
+        overlayController.SetClickThrough(true);
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
         overlayController.SetClickThrough(false);
     }
-
-    IEnumerator CheckRaycast()
+    public void OnPointerExit(PointerEventData eventData)
     {
-        //Set up the new Pointer Event
-        var m_PointerEventData = new PointerEventData(eventSystem);
-        while (true)
-        {
-            //Set the Pointer Event Position to that of the mouse position
-            m_PointerEventData.position = Input.mousePosition;
-            //Create a list of Raycast Results
-            List<RaycastResult> results = new List<RaycastResult>();
-            //Raycast using the Graphics Raycaster and mouse click position
-            graphicRaycaster.Raycast(m_PointerEventData, results);
-
-            bool hit = results.Count > 0;
-            if(hit != status)
-            {
-                status = hit;
-                overlayController.SetClickThrough(!hit);
-            }
-            yield return new WaitForSeconds(0.1f);
-        }
+        overlayController.SetClickThrough(true);
     }
 }
